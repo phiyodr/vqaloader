c4c43e9ecfab61784dbc2d5b5e7c27715dd93f27
Philipp <phiyodr@gmail.com>
2022-04-27 15:32:01 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+with open('requirements.txt') as file:
+    required = file.read().splitlines()
+    
+setuptools.setup(
+    name="vqaloader",
+    version="0.1",
+    author="Philipp J. Roesch",
+    author_email="philipp.roesch@unibw.de",
+    description="vqaloader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phiyodr/vqaloader",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition"
+    ],
+    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    python_requires='>=3.6',
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.csv", "*.json"]},
+    install_requires=required
+    )
