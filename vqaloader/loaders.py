245dd6b393de048b1129af68fccb944b6bde7955
Philipp <phiyodr@gmail.com>
2022-05-02 16:41:22 +0200

    Add VQAv2.0 dataset

--- vqaloader/loaders.py
+++ vqaloader/loaders.py
@@ -3,6 +3,7 @@ import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import time
+import json
 
 ########################################################################################
 # GQA
@@ -177,3 +178,105 @@ if __name__ == '__main__':
     print("===TextVQADataset===")
     dataset = TextVQADataset(split="train",  data_path="~/Data/TextVQA", tokenize=clip_tokenize, testing=False)
     print(dataset[0])
+
+
+
+########################################################################################
+# VQAv2 https://visualqa.org/download.html
+########################################################################################
+
+def most_common_from_dict(dct):
+    lst = [x["answer"] for x in dct]
+    return max(set(lst), key=lst.count)
+
+class VQAv2Dataset(Dataset):
+    IMAGE_PATH = {"train": ("train2014", "v2_OpenEnded_mscoco_train2014_questions.json", "v2_mscoco_train2014_annotations.json"), 
+                  "val": ("val2014", "v2_OpenEnded_mscoco_val2014_questions.json", "v2_mscoco_val2014_annotations.json"),  
+                  "testdev": ("test2015", "v2_OpenEnded_mscoco_test-dev2015_questions.json"), 
+                  "test": ("test2015", "v2_OpenEnded_mscoco_test2015_questions.json")}
+    
+    def __init__(self, split, data_path="",
+                 image_transforms=None, question_transforms=None, tokenize=None,
+                 answer_selection=most_common_from_dict,
+                 verbose=True, testing=False):
+        """
+        split train, val, test
+        balanced True, False
+        image_transforms
+        question_transforms
+        """
+        start_time = time.time()
+        self.split = split
+        self.testing = testing
+        self.answer_selection = answer_selection
+        assert split in ["train", "val", "testdev", "test"]
+        self.data_path = data_path
+        self.image_transforms = image_transforms
+        self.question_transforms = question_transforms
+        self.tokenize = tokenize
+
+        if verbose:
+            path = ""
+            print(f"Start loading VQAv2 Dataset from {path}", flush=True)
+        
+        # Questions
+        path = os.path.expanduser(os.path.join(data_path, self.IMAGE_PATH[split][1]))
+        with open(path, 'r') as f:
+            data = json.load(f)
+        df = pd.DataFrame(data["questions"])
+        df["image_path"] = df["image_id"].apply(lambda x: f"{self.IMAGE_PATH[split][0]}/COCO_{self.IMAGE_PATH[split][0]}_{x:012d}.jpg")
+        
+        # Annotations
+        if not testing:
+            path = os.path.expanduser(os.path.join(data_path, self.IMAGE_PATH[split][2]))
+            with open(path, 'r') as f:
+                data = json.load(f)   
+            df_annotations = pd.DataFrame(data["annotations"])
+            df = pd.merge(df, df_annotations, left_on='question_id', right_on='question_id', how='left')
+            df["image_id"] = df["image_id_x"]
+            if not all(df["image_id_y"] == df["image_id_x"]):
+                print("There is something wrong with image_id")
+            del df["image_id_x"]
+            del df["image_id_y"]
+        self.df = df
+        self.n_samples = self.df.shape[0]
+        if verbose:
+            print(f"Loading VQAv2 Dataset done in {time.time()-start_time:.1f} seconds. Loaded {self.n_samples} samples.")
+        
+    def __getitem__(self, index):
+        # image input
+        image_id = self.df.iloc[index]["image_id"]
+        image_path = self.df.iloc[index]["image_path"]
+        question = self.df.iloc[index]["question"]
+        question_id = self.df.iloc[index]["question_id"]
+        split = self.split
+        if not self.testing:
+            main_answer = self.df.iloc[index]["multiple_choice_answer"]
+            answers = self.df.iloc[index]["answers"]
+            selected_answers = self.answer_selection(self.df.iloc[index]["answers"])
+        
+        # Load and transform image
+        image_path = os.path.expanduser(os.path.join(self.data_path, image_path))        
+        #print("image_path ---->", image_path)
+        with open(image_path, "rb") as f:
+            img = Image.open(f)
+        if self.image_transforms:
+            img = self.image_transforms(img)
+
+        # Load, transform and tokenize question
+        if self.question_transforms: 
+            question = self.question_transforms(question)
+        if self.tokenize:
+            question = self.tokenize(question)
+
+        # Return
+        if self.testing:
+            return question_id, img, question
+        else:
+            return question_id, main_answer, answers, selected_answers, img, question
+
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
+    
+    
